62e483b1a271cc1fc5880709a170de112e5d4d99
Levin Jian <jianzhirong@gmail.com>
2018-01-26 15:09:18 +0800

    add batch norm and dropout

--- nets/spn_cnn.py
+++ nets/spn_cnn.py
@@ -18,6 +18,7 @@ def spn_cnn(inputs,
                      is_training=True,
                      prediction_fn=tf.contrib.layers.softmax,
                      reuse=None,
+                     keep_prob = 0.5,
                      scope='spncnn'):
     
     input_shape = inputs.get_shape().as_list()
@@ -28,51 +29,54 @@ def spn_cnn(inputs,
     with tf.variable_scope(scope, [inputs], reuse=reuse) as scope:
         with slim.arg_scope([slim.batch_norm, slim.dropout],
                                                 is_training=is_training):
-            ##############################
-            #   LOCALIZATION NETWORK
-            ##############################
-            net = inputs
-            net = slim.max_pool2d(net, [2, 2])
-            net = slim.conv2d(net, 20, [3, 3], scope='l_conv0_loc')
-            net = slim.max_pool2d(net, [2, 2])
-            net = slim.conv2d(net, 20, [3, 3], scope='l_conv1_loc')
-            net = slim.max_pool2d(net, [2, 2])
-            net = slim.conv2d(net, 20, [3, 3], scope='l_conv2_loc')
-            
-            net = slim.flatten(net)
-            net = slim.fully_connected(net, 200, scope='l_dense_loc')
-            
-            # INIT TRANSFORM TO IDENTITY
-            b = np.zeros((2, 3), dtype='float32')
-            b[0, 0] = 1
-            b[1, 1] = 1
-            
-            theta = slim.fully_connected(net, 6, scope='A_net',weights_initializer = tf.zeros_initializer(), 
-                                       biases_initializer=tf.constant_initializer(b.flatten()),activation_fn=None)
-            input_fmap = inputs
-            
-            H = input_shape[1]
-            W = input_shape[2]
-#             out_dims = [tf.cast(H/2, tf.int32),tf.cast( W/2, tf.int32)]
-            out_dims = [int(H/2), int(W/2)]
-            net = spatial_transformer_network(input_fmap, theta, out_dims=out_dims)
-            
-            
-            #classification network
-            net = slim.conv2d(net, 96, [3, 3])
-            net = slim.max_pool2d(net, [2, 2])
-            net = slim.conv2d(net, 96, [3, 3])
-            net = slim.max_pool2d(net, [2, 2])
-            net = slim.conv2d(net, 96, [3, 3])
-            net = slim.conv2d(net, 96, [3, 3])
-            
-            net = slim.flatten(net)
-            net = slim.fully_connected(net, 400)
-            
-            logits = slim.fully_connected(net, num_classes,activation_fn=None)
-            end_points['Logits'] = logits
-            if prediction_fn:
-                end_points['Predictions'] = prediction_fn(logits, scope='Predictions')
+            with slim.arg_scope([slim.dropout],
+                                                keep_prob=keep_prob):
+                ##############################
+                #   LOCALIZATION NETWORK
+                ##############################
+                net = inputs
+                net = slim.max_pool2d(net, [2, 2])
+                net = slim.conv2d(net, 20, [3, 3], scope='l_conv0_loc')
+                net = slim.max_pool2d(net, [2, 2])
+                net = slim.conv2d(net, 20, [3, 3], scope='l_conv1_loc')
+                net = slim.max_pool2d(net, [2, 2])
+                net = slim.conv2d(net, 20, [3, 3], scope='l_conv2_loc')
+                
+                net = slim.flatten(net)
+                net = slim.fully_connected(net, 200, scope='l_dense_loc')
+                net = slim.dropout(net)
+                
+                # INIT TRANSFORM TO IDENTITY
+                b = np.zeros((2, 3), dtype='float32')
+                b[0, 0] = 1
+                b[1, 1] = 1
+                
+                theta = slim.fully_connected(net, 6, scope='A_net',weights_initializer = tf.zeros_initializer(), 
+                                           biases_initializer=tf.constant_initializer(b.flatten()),activation_fn=None)
+                input_fmap = inputs
+                
+                H = input_shape[1]
+                W = input_shape[2]
+    #             out_dims = [tf.cast(H/2, tf.int32),tf.cast( W/2, tf.int32)]
+                out_dims = [int(H/2), int(W/2)]
+                net = spatial_transformer_network(input_fmap, theta, out_dims=out_dims)
+                
+                
+                #classification network
+                net = slim.conv2d(net, 96, [3, 3])
+                net = slim.max_pool2d(net, [2, 2])
+                net = slim.conv2d(net, 96, [3, 3])
+                net = slim.max_pool2d(net, [2, 2])
+                net = slim.conv2d(net, 96, [3, 3])
+                net = slim.conv2d(net, 96, [3, 3])
+                
+                net = slim.flatten(net)
+                net = slim.fully_connected(net, 400)
+                net = slim.dropout(net)
+                logits = slim.fully_connected(net, num_classes,activation_fn=None)
+                end_points['Logits'] = logits
+                if prediction_fn:
+                    end_points['Predictions'] = prediction_fn(logits, scope='Predictions')
             
             
     return logits, end_points
@@ -111,7 +115,7 @@ def ffn_spn_arg_scope(is_training=True,
         depthwise_regularizer = None
     with slim.arg_scope([slim.conv2d, slim.separable_conv2d, slim.fully_connected],
                                             weights_initializer=weights_init,
-                                            activation_fn=tf.nn.relu, normalizer_fn=None):
+                                            activation_fn=tf.nn.relu, normalizer_fn=slim.batch_norm):
         with slim.arg_scope([slim.batch_norm], **batch_norm_params):
             with slim.arg_scope([slim.conv2d], weights_regularizer=regularizer):
                 with slim.arg_scope([slim.separable_conv2d],
